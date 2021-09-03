a8211d970f12585b54af45a7cb1930399f07d6f3
Quinten Kasteel <kasteel@floydhamilton.nl>
2021-09-03 22:15:54 +0200

    generate script

--- generate.hs
+++ generate.hs
@@ -9,44 +9,50 @@ import Data.Text (Text)
 import Data.Vector (Vector)
 import Data.Yaml
 
-data Person = Person
-  { personName :: !Text,
-    personAge :: !Int
-  }
-  deriving (Show, Eq)
+-- data Person = Person
+--   { personName :: !Text,
+--     personAge :: !Int
+--   }
+--   deriving (Show, Eq)
 
--- Could use Generic deriving, doing it by hand
-instance ToJSON Person where
-  toJSON Person {..} =
-    object
-      [ "name" .= personName,
-        "age" .= personAge
-      ]
+-- -- Could use Generic deriving, doing it by hand
+-- instance ToJSON Person where
+--   toJSON Person {..} =
+--     object
+--       [ "name" .= personName,
+--         "age" .= personAge
+--       ]
 
-instance FromJSON Person where
-  parseJSON = withObject "Person" $ \o ->
-    Person
-      <$> o .: "name"
-      <*> o .: "age"
+-- instance FromJSON Person where
+--   parseJSON = withObject "Person" $ \o ->
+--     Person
+--       <$> o .: "name"
+--       <*> o .: "age"
+urls :: [String]
+urls =
+  [ "https://quintenkasteel.github.io/visual-regression-test"
+  ]
+
+file :: String
+file = "snapshots.yaml"
 
 main :: IO ()
 main = do
-  let bs =
-        encode
-          [ Person "Alice" 25,
-            Person "Bob" 30,
-            Person "Charlie" 35
-          ]
-  people <-
-    case decodeEither' bs of
-      Left exc -> error $ "Could not parse: " ++ show exc
-      Right people -> return people
+  encodedUrls <- encodeUrls urls
+  encodeFile file (encodedUrls :: Vector Text)
+  resDecoder encodedUrls
 
-  let fp = "people.yaml"
-  encodeFile fp (people :: Vector Person)
-  res <- decodeFileEither fp
+encodeUrls :: [String] -> IO (Vector Text)
+encodeUrls urls =
+  case decodeEither' (encode urls) of
+    Left exc -> error $ "Could not parse: " ++ show exc
+    Right urls' -> return urls'
+
+resDecoder :: Vector Text -> IO ()
+resDecoder encodedUrls = do
+  res <- decodeFileEither file
   case res of
     Left exc -> error $ "Could not parse file: " ++ show exc
-    Right people2
-      | people == people2 -> mapM_ print people
+    Right encodedUrls2
+      | encodedUrls == encodedUrls2 -> mapM_ print encodedUrls
       | otherwise -> error "Mismatch!"
