0f7796657ba45fe0a05ffdbf0680157b9da2e9eb
Quinten Kasteel <kasteel@floydhamilton.nl>
2021-09-04 02:16:36 +0200

    init

--- src/Main.elm
+++ src/Main.elm
@@ -4,22 +4,28 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+main =
+    Browser.sandbox { init = 1, update = update, view = view }
+
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
