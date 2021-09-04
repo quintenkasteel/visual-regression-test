589ae94ccd077dae2a8a5fe52179a6bef48b4d2f
Quinten Kasteel <kasteel@floydhamilton.nl>
2021-09-04 02:01:39 +0200

    add multiple domains

--- generate.hs
+++ generate.hs
@@ -1,37 +1,65 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.21 script
 
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
 
+import Data.String.Interpolate
 import Data.Text (Text)
 import Data.Vector (Vector)
 import Data.Yaml
+import System.Process
 
-urls :: [String]
+data Domain = Domain
+  { name :: String,
+    url :: String,
+    token :: String
+  }
+  deriving (Eq, Show)
+
+urls :: [Domain]
 urls =
-  [ "https://hanos-accept.floydhamilton.net/vacatures",
-    "https://welkoop-accept.floydhamilton.net/vacatures"
+  [ Domain
+      { name = "test",
+        url = "https://quintenkasteel.github.io/visual-regression-test",
+        token = "33ae625f5325f75d6e7166bc913224399f4a411ee23f9e5d7f118b4f7840e20e"
+      }
   ]
 
-file :: String
-file = "snapshots.yaml"
+command :: Domain -> IO ()
+command Domain {name, token} =
+  callCommand [i|#{percy_token token} yarn percy snapshot #{file name}|]
+
+file :: String -> String
+file name = [i|snapshots-#{name}.yaml|]
+
+percy_token :: String -> String
+percy_token value = [i|PERCY_TOKEN=#{value}|]
 
 main :: IO ()
 main = do
-  encodedUrls <- encodeUrls
-  encodeFile file encodedUrls
-  resDecoder encodedUrls
+  _ <-
+    traverse
+      ( \domain'@(Domain {name, url}) -> do
+          encodedUrls <- encodeUrls [url]
+          encodeFile (file name) encodedUrls
+          resDecoder encodedUrls name
+          command domain'
+      )
+      urls
+  return ()
 
-encodeUrls :: IO (Vector Text)
-encodeUrls =
-  case decodeEither' (encode urls) of
+encodeUrls :: [String] -> IO (Vector Text)
+encodeUrls urls' =
+  case decodeEither' (encode urls') of
     Left exc -> error $ "Could not parse: " ++ show exc
-    Right urls' -> return urls'
+    Right urls_ -> return urls_
 
-resDecoder :: Vector Text -> IO ()
-resDecoder encodedUrls = do
-  res <- decodeFileEither file
+resDecoder :: Vector Text -> String -> IO ()
+resDecoder encodedUrls name = do
+  res <- decodeFileEither (file name)
   case res of
     Left exc -> error $ "Could not parse file: " ++ show exc
     Right encodedUrls2
